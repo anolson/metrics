10d38f2fd4ed36940fb7602bdd874b3c0d33365f
Andrew Olson <anolson@gmail.com>
2011-05-03 11:59:08 -0400

    Sync ride metrics from strava.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,28 +1,60 @@
 class Ride < ActiveRecord::Base
   
-  attr_accessor :strava_ride_id
-  attr_writer :watts
+  attr_accessor :watts
+  # attr_writer :watts
   
-  def calculate_normalized_power
-    normalized_power = Joule::Calculator::PowerCalculator.normalized_power(watts, record_interval)
-    write_attribute(:normalized_power, normalized_power)
+  def initialize(options = {})
+    super options
+    @watts = []
   end
   
-  def watts
-    @watts ||= fetch_watts_from_strava
+  def sync(strava_ride_id, threshold_power = 0)
+    @watts = fetch_watts_from_strava(strava_ride_id)
+    calculate_metrics(threshold_power)
+  end
+  
+  def calculate_metrics(threshold_power = 0)
+    calculate_normalized_power
+    calculate_training_stress_score(threshold_power)
+    calculate_intensity_factor(threshold_power)
   end
   
   def record_interval
     1
   end
   
-  def fetch_watts_from_strava
-    fetch_ride_streams_from_strava.watts.collect { |item| item.nil? && 0 || item}
+  def duration_seconds
+    record_interval * watts.size
   end
   
-  def fetch_ride_streams_from_strava
-    strava = StravaApi::Base.new
-    strava.ride_streams(@strava_ride_id)
-  end
+  private
+    
+    def calculate_normalized_power
+      np = Joule::Calculator::PowerCalculator.normalized_power(watts, record_interval)
+      write_attribute(:normalized_power, np)
+    end
+  
+    def calculate_training_stress_score(threshold_power)
+      tss = Joule::Calculator::PowerCalculator.training_stress_score(duration_seconds, self.normalized_power, threshold_power)
+      write_attribute(:training_stress_score, tss)
+    end
+    
+    def calculate_intensity_factor(threshold_power)
+      intensity_factor = Joule::Calculator::PowerCalculator.intensity_factor(self.normalized_power, threshold_power)
+      write_attribute(:intensity_factor, intensity_factor)
+    end
+  
+    def fetch_watts_from_strava(strava_ride_id)
+      streams = fetch_ride_streams_from_strava(strava_ride_id)
+
+      unless(streams.watts.empty?)
+        streams.watts.collect { |item| item.nil? && 0 || item }
+      end
+    end
+    
+    def fetch_ride_streams_from_strava(strava_ride_id)
+      strava = StravaApi::Base.new
+      strava.ride_streams(strava_ride_id)
+    end
   
 end
--- test/unit/ride_test.rb
+++ test/unit/ride_test.rb
@@ -2,51 +2,48 @@ require 'test_helper'
 
 class RideTest < ActiveSupport::TestCase
   
-  test "test a new ride without any metrics" do
+  test "creating a new ride without any metrics" do
     ride = Ride.new
     assert_equal 0.0, ride.normalized_power
     assert_equal 0.0, ride.training_stress_score
     assert_equal 0.0, ride.intensity_factor
   end
   
-  test "test a new ride with metrics" do
+  test "creating a new ride with metrics" do
     ride = Ride.new :normalized_power => 234.0, :training_stress_score => 90.3, :intensity_factor => 0.8
     assert_equal 234.0, ride.normalized_power
     assert_equal 90.3, ride.training_stress_score
     assert_equal 0.8, ride.intensity_factor
   end
   
-  test "test find a ride without any metrics" do
+  test "finding a ride without any metrics" do
     ride = Ride.find(1)
     assert_equal 0.0, ride.normalized_power
     assert_equal 0.0, ride.training_stress_score
     assert_equal 0.0, ride.intensity_factor
   end
   
-  test "test find a ride with metrics" do
+  test "finding a ride with metrics" do
     ride = Ride.find(2)
     assert_equal 234.0, ride.normalized_power
     assert_equal 90.3, ride.training_stress_score
     assert_equal 0.8, ride.intensity_factor
   end
-  
-  test "test normalized power calculation for a ride without power values" do
-    ride = Ride.new 
-    ride.watts = []
-    ride.calculate_normalized_power
-    assert_equal 0.0, ride.normalized_power
+
+  test "calculate metrics for a ride without any power values" do 
+    ride = Ride.new
+    ride.calculate_metrics(320)
+    assert_equal 0.0, ride.normalized_power.round(2)
+    assert_equal 0.0, ride.training_stress_score.round(2)
+    assert_equal 0.0, ride.intensity_factor.round(1)
   end
 
-  test "fetch watts for a ride from strava" do
-    ride = Ride.new :strava_ride_id => 486125
-    ride.fetch_watts_from_strava
-    assert_not_nil ride.watts
-  end
-  
-  test "test normalized power calculation for a ride fetched from strava" do
-    ride = Ride.new :strava_ride_id => 486125
-    ride.calculate_normalized_power
+  test "syncing a ride from strava" do 
+    ride = Ride.new
+    ride.sync(486125, 320)
     assert_equal 288.46, ride.normalized_power.round(2)
+    assert_equal 148.68, ride.training_stress_score.round(2)
+    assert_equal 0.9, ride.intensity_factor.round(1)
   end
   
 end
