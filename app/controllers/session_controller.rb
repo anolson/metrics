c546687fb890952f64df77bcd983e33f339c1e49
Andrew Olson <anolson@gmail.com>
2011-05-10 14:06:30 -0400

    Adding SessionController.

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,18 @@
+class SessionController < ApplicationController
+  
+  def new; end
+  
+  def create
+    user = User.authenticate(params[:user])      
+    session[:user] = user.id
+    redirect_to(@user)
+  rescue
+    redirect_to signin_path, :notice => 'Invalid email or password.'
+  end
+  
+  def destroy
+    reset_session
+    redirect_to root_path
+  end
+  
+end
