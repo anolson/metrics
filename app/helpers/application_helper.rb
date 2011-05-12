15eaa8203c26bbabf08f27cbf42d417392a43aae
Andrew Olson <anolson@gmail.com>
2011-05-12 13:40:58 -0400

    Adding header and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def link_to_login_logout
+    if has_authenticated?
+      link_to "Logout", logout_path
+    else
+      link_to "Login", login_path
+    end
+  end
+  
 end
