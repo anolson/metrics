01a32fb496510de680d26843e36f788294251a89
Andrew Olson <anolson@gmail.com>
2011-09-26 17:03:31 -0400

    Fixing routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Metrics::Application.routes.draw do
   resource :users, :only => [:new, :create] 
   resources :rides, :only => [:index, :show]
       
-  match "get_started" => 'users#new', :constraints => { :protocol => Rails.env.production? ? 'https' : 'http'}
-  match "login" => 'session#new', :constraints => { :protocol => Rails.env.production? ? 'https' : 'http'}
+  match "get_started" => 'users#new'
+  match "login" => 'session#new'
   match "logout" => 'session#destroy'
   
   root :to => "rides#index"
